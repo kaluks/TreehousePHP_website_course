a906072c22fd052e0bc06a4be87424c239a71eb8
Kathryn <kanne1126@gmail.com>
2016-09-26 23:59:20 -0400

    first commit

---
+++ thanks.php
@@ -0,0 +1,11 @@
+<?php
+$pageTitle = "Thank you";
+$section = null;
+?>
+
+<div class="section page">
+  <h1>Thank You</h1>
+  <p>Thanks for your email</p>
+</div>
+
+<?php include("inc.footer.php"); ?>
